0ad1a37576c2fea84bff259c9c37937077c1534c
Mattias <tartuvali@gmail.com>
2024-03-29 17:59:59 +0200

    Veerud sisaldav nüüd listi mis annab täpselt teada mis tabeleid ta sisendina kasutab

---
+++ out/production/Java_Projekt_2024_SQL_Pars/src/main/java/Tabel.java
@@ -0,0 +1,24 @@
+package main.java;
+
+public class Tabel {
+    String tabelNimi;
+    String tabelAlias;
+
+    @Override
+    public String toString() {
+        return "Tabel{" +
+                "tabelNimi='" + tabelNimi + '\'' +
+                ", tabelAlias='" + tabelAlias + '\'' +
+                '}';
+    }
+
+
+
+    public Tabel(String tabelNimi, String tabelAlias) {
+        this.tabelNimi = tabelNimi;
+        this.tabelAlias = tabelAlias;
+    }
+
+
+
+}
