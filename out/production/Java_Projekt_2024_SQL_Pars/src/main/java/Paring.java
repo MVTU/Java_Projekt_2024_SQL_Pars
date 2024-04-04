f4e0614bc53149819912783e22a9b0dbae6a5067
Mattias <tartuvali@gmail.com>
2024-04-04 10:21:13 +0300

    mainis on nüüd selgemalt välja toodud kuidas andmeid töödelda

--- out/production/Java_Projekt_2024_SQL_Pars/src/main/java/Paring.java
+++ out/production/Java_Projekt_2024_SQL_Pars/src/main/java/Paring.java
@@ -40,9 +40,12 @@ public class Paring {
             //Otsi sulu algust, et mitte arvestada sealseid komasid
              if (suluAlgus.equals(String.valueOf(valjund.charAt(i)))) {
                     suluInd = true;
-
                     //Kui pole sulgude sees ja on näha koma siis arvesta veeruks
-                } else if (koma.equals(String.valueOf(valjund.charAt(i))) && !suluInd) {
+                }
+                else if  (suluLõpp.equals(String.valueOf(valjund.charAt(i)))) {
+                 suluInd = false;
+             }
+                else if (koma.equals(String.valueOf(valjund.charAt(i))) && !suluInd) {
                     reaLõpp = i;
 
                     // Eemalda select osa, eeldus on et sisseloetud valjund algab ilma tühikuteta
@@ -50,11 +53,18 @@ public class Paring {
                     if (selekt.equalsIgnoreCase(selectTükk[0])) {
                         reaAlgus = reaAlgus + 6;
                     }
-                    Veerg uusVeerg = new Veerg(valjund.substring(reaAlgus, reaLõpp));
+
+                    Veerg uusVeerg = new Veerg(valjund.substring(reaAlgus, reaLõpp).trim());
                     uusVeerg.leiaSisendTabel();
                     veerud.add(uusVeerg);
-                    reaAlgus = i;
+                    reaAlgus = i + 1;
                 }
+                else if (i == valjund.length() -1) {
+                    reaLõpp = i;
+                 Veerg uusVeerg = new Veerg(valjund.substring(reaAlgus, reaLõpp).trim());
+                 uusVeerg.leiaSisendTabel();
+                 veerud.add(uusVeerg);
+             }
 
         }
 
@@ -81,11 +91,11 @@ public class Paring {
                 if (from.equalsIgnoreCase(tükid[i])) {
                     tabel = tükid[i + 1];
                     alias = tükid[i + 2];
-                    tabelid.add(new Tabel(tabel,alias));
+                    tabelid.add(new Tabel(null,alias,tabel));
                 } else if (join.equalsIgnoreCase(tükid[i])) {
                     tabel = tükid[i + 1];
                     alias = tükid[i + 2];
-                    tabelid.add(new Tabel(tabel,alias));
+                    tabelid.add(new Tabel(null,alias,tabel));
                 }
 
         }
--- out/production/Java_Projekt_2024_SQL_Pars/src/main/java/Veerg.java
+++ out/production/Java_Projekt_2024_SQL_Pars/src/main/java/Veerg.java
@@ -5,26 +5,74 @@ import java.util.ArrayList;
 public class Veerg {
 
     public String veeruKood;
-           public ArrayList<Tabel> allikasTabelid = new ArrayList<>();
+    public ArrayList<Tabel> allikasTabelid = new ArrayList<>();
+    public String nimi;
 
 
     public Veerg(String veeruKood) {
         this.veeruKood = veeruKood;
     }
 
-    public ArrayList<Tabel> leiaSisendTabel(){
+    public String getVeeruKood() {
+        return veeruKood;
+    }
+
+    public void setVeeruKood(String veeruKood) {
+        this.veeruKood = veeruKood;
+    }
+
+    public ArrayList<Tabel> getAllikasTabelid() {
+        return allikasTabelid;
+    }
+
+    public void setAllikasTabelid(ArrayList<Tabel> allikasTabelid) {
+        this.allikasTabelid = allikasTabelid;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public ArrayList<Tabel> leiaSisendTabel() {
         String[] sõned = veeruKood.split(" ");
         for (int i = 0; i < sõned.length; i++) {
             String[] sõne = sõned[i].split("\\.");
-            if (sõne.length == 2){
-            String alias = sõne[0];
-            String tabel = sõne[1];
-            Tabel uus = new Tabel(tabel,alias);
-            allikasTabelid.add(uus);
+            if (sõne.length == 2) {
+                String[] aliasSplit = sõne[0].split("\\(") ;
+                String alias;
+                if(aliasSplit.length > 1){
+                    alias = aliasSplit[aliasSplit.length-1];
+                }
+                else {
+                    alias = sõne[0];
+                }
+
+                String tabel = sõne[1];
+                Tabel uus = new Tabel(tabel, alias,null);
+                allikasTabelid.add(uus);
+                if (1 == sõned.length) {
+                    nimi = tabel;
+                    nimi = nimi.trim();
+                }
             }
         }
+        if (sõned.length > 2) {
+            if (sõned[sõned.length - 2].trim().equalsIgnoreCase("AS")) {
+                nimi = sõned[sõned.length - 1];
+                nimi = nimi.trim();
+            }
+        }
+
         return  allikasTabelid;
         }
+
+
+
+
 
 
 
@@ -33,6 +81,7 @@ public class Veerg {
         return "Veerg{" +
                 "veeruKood='" + veeruKood + '\'' +
                 ", allikasTabel='" + allikasTabelid + '\'' +
+                ", väljundVeerg='" + nimi + '\'' +
                 '}';
 
     }
